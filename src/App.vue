6e0db5d47f113959d429ad941ee193c413b84b45
Nayana Ramesh <107012@tfcorp.com>
2022-10-07 17:21:42 +0530

    vuetify files

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+   <!-- <navbar-comp /> -->
+   <form-comp/>
+  </v-app>
+</template>
+
+<script>
+import FormComp from './components/FormComp.vue';
+// import NavbarComp from './components/NavbarComp.vue';
+
+export default {
+  name: 'App',
+
+  components: {
+    // NavbarComp,
+    FormComp
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
